0c37d744d38cf5ccd46a399f8cb6e9d18cff0452
Jobizzness <jobizzness@gmail.com>
2016-02-05 23:13:12 +0000

    hello

---
+++ config/connection.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jobizzness
+ * Date: 1/13/2016
+ * Time: 2:09 PM
+ */
+
+#Database Conncection
+$hostname = 'localhost';$username = 'mtdeveloper';$password = 'MTLkf&4v903*%^$juicjio';$DB_name = "mytailor";
+
+try {
+    $dbc = new PDO("mysql:host=$hostname;dbname=$DB_name", $username, $password);
+}
+catch(PDOException $e)
+{
+    echo $e->getMessage();
+}
